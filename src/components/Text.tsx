62b02f59502df85be79c45d9fe5a364576470283
johann <johann.lindele@tn.mischok.academy>
2025-06-27 14:58:07 +0200

    Text hinzugefügt

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -2,6 +2,9 @@
 import React from "react";
 // @ts-ignore
 import BGDev from "../assets/keyboardBG.jpg";
+import {FaFilePdf} from "react-icons/fa";
+// @ts-ignore
+import InfoIHK from "../assets/Information Praktikumsbetriebe.pdf";
 
 const Text = () => {
     return (
@@ -31,6 +34,22 @@ const Text = () => {
                 </p>
                 <p className="text-xl md:text-2xl text-gray-800 leading-relaxed font-semibold mt-8">
                     Gerne überzeuge ich Sie in einem <span className="text-blue-600">persönlichen Gespräch</span> von meiner <span className="text-blue-600">Motivation und Eignung</span>. Ich freue mich auf Ihre <span className="text-blue-600">Einladung</span>.
+                    <div className="max-w-3xl mx-auto rounded-xl shadow-xl p-8 md:p-10 text-center relative overflow-hidden
+               bg-white/70 transition-all duration-300 ease-in-out hover:scale-105 hover:bg-white/100">
+                        {/* Container für den Download der Praktikumsinformationen der IHK*/}
+                        <p className="text-md text-gray-600 mb-4">
+                            Möchten Sie mehr erfahren? Laden Sie die Praktikumsinformationen der IHK herunter:
+                        </p>
+                        <a
+                            href={InfoIHK}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="inline-flex items-center justify-center px-6 py-3 border border-transparent text-base font-medium rounded-full text-white bg-red-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition-all duration-300 shadow-lg transform hover:-translate-y-1 hover:scale-105"
+                        >
+                            <FaFilePdf className="w-5 h-5 mr-2" />
+                            Praktikumsinformation herunterladen
+                        </a>
+                    </div>
                 </p>
             </div>
         </section>
